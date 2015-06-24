98878e21efc43d327c53fe7aeb8a84273b78f9a4
jiaowoshabi <wenhaoz@usc.edu>
2015-06-23 21:14:13 -0700

    Update Merge Two Sorted Lists(better).java

--- Merge Two Sorted Lists/Merge Two Sorted Lists(better).java
+++ Merge Two Sorted Lists/Merge Two Sorted Lists(better).java
@@ -10,7 +10,7 @@ public class Solution {
  public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 	        if(l2==null) return l1;
 	        if(l1==null ) return l2;
-	        ListNode current=new ListNode(-1);
+	        ListNode current=new ListNode(-1);//create a fake header of the list
 	        ListNode head=current;
 	        while(l1!=null && l2!=null){
 	            if(l1.val>l2.val ){
@@ -24,8 +24,8 @@ public class Solution {
 	                current=current.next;
 	            }
 	        }
-	        if(l1!=null) current.next=l1;
-	         if(l2!=null) current.next=l2;
+	        if(l1!=null) current.next=l1;//append l1 if l2 run out of nodes
+	         if(l2!=null) current.next=l2;//append l2 if l1 run out of nodes
 	        return head.next;
 	    }
 }
